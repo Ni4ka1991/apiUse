34d6876b89f5abbc00642c1d27d68f7bbf7b7978
Ni4ka1991 <astahova_nica@mail.ru>
2021-04-25 17:28:33 +0300

    Add file app.py

--- app.py
+++ app.py
@@ -9,13 +9,8 @@ from data import *
 
 data, query = importData()
 
-print( query )
-print(type(data))
-print(len(data))
-input("hit enter to continue ... ")
 
 if( data['status'] == "success" ):
-# pass
  paginator( 1 )
 else:
  print( "ERROR: ", data['message'])
