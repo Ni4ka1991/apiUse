7d64ef871c5d819e387ecf35cadbc43a5a6bbd2a
Ni4ka1991 <astahova_nica@mail.ru>
2021-05-09 11:34:06 +0300

    Add file app.py

--- app.py
+++ app.py
@@ -9,10 +9,7 @@ from data import *
 
 def viewInfo( ):
     if( data['status'] == "success" ):
-        if( paginator( 1 ) == 0 ):
-            print( "See you later!" )
-        else:
-            paginator( 1 )
+        paginator( 1 )
     else:
         print( "ERROR: ", data['message'])
 
